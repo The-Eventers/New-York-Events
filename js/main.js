61311cfafca7e12f7ae6ed3dd68fcface24d1d94
Jess Taylor <jessicaeliset@gmail.com>
2015-08-07 10:48:58 -0400

    fixing merge conflicts

--- js/main.js
+++ js/main.js
@@ -15,10 +15,7 @@
 var app = {};
 app.neighborhood = '';
 app.category = '';
-<<<<<<< HEAD:js/main.js
 app.subcategory = '';
-=======
->>>>>>> b90a5a202db2d8dbae8e0d77619b765cf13ac28a:js/main.js
 
 
 app.init = function () {
@@ -33,7 +30,6 @@ app.init = function () {
 		    console.log(app.neighborhood);
 		  });
 		$('.category-question input[type=checkbox]:checked').each(function() {
-<<<<<<< HEAD:js/main.js
 			if ($(this).hasClass('subcategory')) { //if option selected is a subcategory (museums or events) then store that value as subcategory
 				app.subcategory = app.subcategory + ' ' + $(this).val();
 			} else if ($(this).hasClass('category')) { //if option selected is a category (comedy, art, theater) then store that value as category
@@ -41,23 +37,19 @@ app.init = function () {
 			}
 			console.log(app.category);
 			console.log(app.subcategory);
-=======
 			app.category = app.category + ' ' + $(this).val();
 			console.log(app.category);
->>>>>>> b90a5a202db2d8dbae8e0d77619b765cf13ac28a:js/main.js
 		});
 		var startDate = $('.start-date').datepicker('getDate');
 		var endDate = $('.end-date').datepicker('getDate');
 		var dateRange = moment(startDate).format('YYYY-MM-DD') + ':' + moment(endDate).format('YYYY-MM-DD');
 		console.log(dateRange);
-<<<<<<< HEAD:js/main.js
 		app.getInfo(dateRange, app.category, app.subcategory, app.neighborhood);
 	});
 };
 
 
 app.getInfo = function(dateRange, category, subcategory, neighborhood) {
-=======
 		app.getInfo(dateRange, app.category, app.neighborhood);
 	});
 };
@@ -69,7 +61,6 @@ L.mapbox.accessToken = 'pk.eyJ1Ijoiam9hbm5hc3RlY2V3aWN6IiwiYSI6IjIzNmNhNjJmNzgxM
 
 
 app.getInfo = function(dateRange, category, neighborhood) {
->>>>>>> b90a5a202db2d8dbae8e0d77619b765cf13ac28a:js/main.js
 	$.ajax({
 		url: 'http://api.nytimes.com/svc/events/v2/listings.jsonp?&filters=neighborhood: (Chelsea "Greenwhich Village" SoHo TriBeCa NoHo)',
 		type: 'GET',
@@ -77,11 +68,7 @@ app.getInfo = function(dateRange, category, neighborhood) {
 	    data: {
 	      'api-key': 'e6a25b3f20881562c56e3247ecd6335d:3:72623857',
 	      'facets': 1,
-<<<<<<< HEAD:js/main.js
-	      'filters': 'category:' + app.category + ',subcategory:' + app.subcategory + ',neighborhood:' + app.neighborhood,
-=======
 	      'filters': 'category:' + app.category + ',neighborhood:' + app.neighborhood,
->>>>>>> b90a5a202db2d8dbae8e0d77619b765cf13ac28a:js/main.js
 	      'limit': 20,
 	      'date_range': dateRange
 	  },
